3b4a290e554f1b451d225dae4b274a527b43f3ae
Joe Burgess <joeezmail@gmail.com>
2023-09-17 08:06:02 -0700

    complete 09-chain-map-js

--- problems/09-chain-map.js
+++ problems/09-chain-map.js
@@ -29,12 +29,33 @@ console.log(chainMap(4, square, half));         // 8
 console.log(chainMap(4, half, square));         // 4
 *******************************************************************************/
 
-let chainMap = function() {
+let chainMap = function(int, ...cbs) {
+    for (let ele of cbs) {
+	int = ele(int);
+    };
 
+    return int;
 };
 
 
 
+let add5 = function(n) {
+    return n + 5;
+};
+
+let half = function(n) {
+    return n / 2;
+};
+
+let square = function(n) {
+    return n * n;
+};
+
+console.log(chainMap(25, add5));                // 30
+console.log(chainMap(25, add5, half));          // 15
+console.log(chainMap(25, add5, half, square));  // 225
+console.log(chainMap(4, square, half));         // 8
+console.log(chainMap(4, half, square));         // 4
 
 
 
